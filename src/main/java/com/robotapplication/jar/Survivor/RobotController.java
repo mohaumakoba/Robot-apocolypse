e0709dcb57c010477b9c7b8893eeff3d4c1329c2
mohaumakoba <118047162+mohaumakoba@users.noreply.github.com>
2024-02-01 16:02:22 +0200

    updating RobotController

--- src/main/java/com/robotapplication/jar/Survivor/RobotController.java
+++ src/main/java/com/robotapplication/jar/Survivor/RobotController.java
@@ -1,19 +1,41 @@
 package com.robotapplication.jar.Survivor;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
-@RequestMapping("https://robotstakeover20210903110417.azurewebsites.net/robotcpu")
+@RequestMapping("robotCpu")
 
 public class RobotController {
-    @Autowired
-    public SurvivorBusinesslogic survivorBusinesslogic;
+    @Value("${external.api.url}")
 
-//  Get all robot and their categories
+   private String apiUrl;
 
+    @GetMapping("/robotList")
+
+    public ResponseEntity<String> getRobotList() {
+
+        WebClient webClient = WebClient.create();
+
+        String result = webClient.get()
+
+              .uri(apiUrl)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+       System.out.println("----------------");
+       System.out.println(result);
+       System.out.println("-----------");
+
+       return ResponseEntity.ok()
+                .header("Content-Type", "application/json")
+
+               .body(result);
+
+    }
 
 }
